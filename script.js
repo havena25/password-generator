8d7bcb6d0500a7b6d192c40db4edc2942fbdeef2
Alia Haven <aliahavenart@gmail.com>
2022-01-08 12:04:24 +0000

    commit on main

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var password=document.getElementsByTagName("textarea");
-var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passwordLength = 8;
-var password = "";
-
 
 // Write password to the #password input
 function writePassword() {
   var password = writePassword();
   var passwordText = document.querySelector("#password");
-
+   
   passwordText.value = password;
 
 }
-for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber, randomNumber +1);
- }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
